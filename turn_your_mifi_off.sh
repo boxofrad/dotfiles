93b6268c09822c4d3d8076ff460c0f31f9e2b5e6
Daniel Upton <daniel@floppy.co>
2014-01-24 16:01:36 +0000

    I *always* forget to turn my MiFi off

---
+++ turn_your_mifi_off.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s | grep "Pretty fly for a WiFi" >> /dev/null
+
+if [ $? -eq 0 ]; then
+  /Users/daniel/.rbenv/shims/terminal-notifier -message "You forgot to turn your MiFi off!" -title "Hey Idiot"
+fi
