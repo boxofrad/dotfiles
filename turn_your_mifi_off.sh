4901ffd1b6543c2df2ff8141df38adfa9b172297
Daniel Upton <daniel@floppy.co>
2015-09-08 07:36:45 +0100

    Remove some old sillyness

--- turn_your_mifi_off.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s | grep "Pretty fly for a WiFi" >> /dev/null
-
-if [ $? -eq 0 ]; then
-  /Users/daniel/.rbenv/shims/terminal-notifier -message "You forgot to turn your MiFi off!" -title "Hey Idiot"
-fi
