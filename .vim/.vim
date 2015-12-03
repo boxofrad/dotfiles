281a6a0af3ce1937b531f00a871b6268c83671a8
Daniel Upton <daniel@floppy.co>
2015-12-03 22:12:22 +0000

    Replace rcm with simple install script, move stuff around a bit

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/Users/daniel/.dotfiles/.vim
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+symlink() {
+  src=$DOTFILES_DIR/$1
+  dst=$HOME/$1
+
+  if [[ -e "$dst" ]]; then
+    echo "EXISTS: $dst"
+  else
+    echo "SYMLINK: $src → $dst"
+    ln -sf "$src" "$dst"
+  fi
+}
+
+# Vim
+symlink .vimrc
+symlink .gvimrc
+symlink .vim
+
+# Git
+symlink .gitconfig
+symlink .git_template
+symlink .gitignore_global
+
+# Tmux
+symlink .tmux.conf
+
+# Ruby
+symlink .irbrc
+
+# ZSH
+symlink .zshrc
+
+# Binaries
+symlink bin
